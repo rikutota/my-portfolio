97856f2af641b4ec7c43a3c3835879f6b68563f5
rikutota <taka010218@gmail.com>
2026-06-26 18:07:04 +0900

    fix : worksの内容追加

--- src/data/works.ts
+++ src/data/works.ts
@@ -2,18 +2,40 @@ export const myProjects = [
   {
     title: "Portfolio Website",
     description:
-      "Next.js, TypeScript, Tailwind CSS, Vercelで作成した自分のポートフォリオサイトです。",
+      "Next.js, TypeScript, Tailwind CSS, Vercelで作成した自分のポートフォリオサイトです.",
     technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Vercel"],
     githubUrl: "https://github.com/rikutota/my-portfolio",
-    demoUrl: "https://my-portfolio-xi-ten-82.vercel.app/",
+  },
+    {
+    title: "Suu",
+    description:
+      "「スマホでも簡単に，見やすく構造化できる」をコンセプトとしたメモ帳アプリです．\n 箇条書き・チェックボックス・drawer機能で簡単に文章を構造化します．",
+    technologies: ["javascript", "firestore database ", "firebase Authentication", "firebase hosting"],
+    webUrl: "https://suu-memoapp.web.app",
   },
 ];
 
+
 export const participatedProjects = [
   {
-    title: "Coming Soon",
+    title: "Billee",
     description:
-      "今後、チーム開発や参加したプロジェクトをここに追加していきます。",
-    technologies: ["Team Development"],
+      "時間指定位置情報共有アプリです．nekonataチーム開発",
+    technologies: ["Mobile App", "flutter"],
+    webUrl: "https://billee.nekonata.dev/",
+  },
+  {
+    title: "Recomen",
+    description:
+      "ラーメン記録，共有アプリです．nekonataチーム開発",
+    technologies: ["Mobile App","flutter"],
+    webUrl: "https://recomen.nekonata.dev/",
+  },
+  {
+    title: "埼大アプリ",
+    description:
+      "埼玉大学生向けの情報支援アプリです．nekonataチーム開発",
+    technologies: ["Mobile App", "flutter"],
+    webUrl: "https://su-app.nekonata.dev/",
   },
 ];
